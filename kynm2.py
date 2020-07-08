e811515d82d10170b02563216d1d13d259fbf999
DeepankRx <49677833+DeepankRx@users.noreply.github.com>
2020-07-08 21:54:06 +0530

    Add files via upload

---
+++ kynm2.py
@@ -0,0 +1,123 @@
+import random
+print("\t\tKnow Your Name Meaning")
+print("1.Play\n2.Exit\n")
+choice=int(input())
+if choice==1:
+	name1=str.upper(input("ENTER YOUR NAME:"))
+	str_lth=len(name1)
+	for i in range(0,str_lth):
+		char1=name1[i]
+		if char1=="A":
+			alist=["Abderian","Ablaze","A1","Awesome","Amazing"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(alist))
+		elif char1=="B":
+			blist=["Balmy","BadBoy","Blooming","Blessed","Baronical"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(blist))
+		elif char1=="C":
+			clist=["Cheerful","Choosy","Caring","Courage","Calm"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(clist))
+		elif char1=="D":
+			dlist=["Dynamic","Daring","Devotional","Delightful","Determined"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(dlist))
+		elif char1=="E":
+	   
+			elist=["Eager","Elegant","Emotive","Enamored","Endeary"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(elist))
+		elif char1=="F":
+			flist=["Fabulous","Fantastic","Facile","Festive","Fortunefull"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(flist))
+		elif char1=="G":
+			glist=["Great","Graceful","Gracios","Glourious","Guilty"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(glist))
+			
+		elif char1=="H":
+			hlist=["Heroic","Hardcore","Hazy","Heated","Helpful"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(hlist))
+		elif char1=="I":
+			ilist=["Iconic","Ideal","Idiotic","Illogical","Immature"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(ilist))
+		elif char1=="J":
+			jlist=["Jealous","Joyful","Jocular","Jesting","Jittery"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(jlist))
+		elif char1=="K":
+			klist=["Kind","Killer","Keen","Kindhearted","Knowledgable"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(klist))
+		elif char1=="L":
+			llist=["Lame","Lazy","Logical","Lonely","Lucky"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(llist))
+		elif char1=="M":
+			mlist=["Mad","Maniac","Messy","Mature","Marvelous"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(mlist))
+		elif char1=="N":
+			nlist=["Nice","Neat","Numb","Natural","Nasty"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(nlist))
+		elif char1=="O":
+			olist=["Obssesive","Offensive","Outstanding","Organized","Otious"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(olist))
+		elif char1=="P":
+			plist=["Pacifist","Pasty","Perfect","Polite","Pretty"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(plist))
+		elif char1=="Q":
+			qlist=["Queasy","Quick","Quiet","Quaint","Quizzed"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(qlist))
+		elif char1=="R":
+			rlist=["Rebel","Rich","Rigid","Rude","Rumped"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(rlist))
+		elif char1=="S":
+			slist=["Scabby","Silent","Screaky","Scurby","Superb"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(slist))
+		elif char1=="T":
+			tlist=["Tearful","Terrific","Tough","Truthful","Tricky Person"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(tlist))
+		elif char1=="U":
+			ulist=["Ugly","Unaffraid","Unlucky","Understanding","Unsure"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(ulist))
+		elif char1=="V":
+			vlist=["Vengeful","Vile","Vivid","Vigorous","Vernal"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(vlist))
+		elif char1=="W":
+			wlist=["Wonderful","Warm","Wild","Wonderfull","Weak"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(w,list))
+		elif char1=="X":
+			xlist=["Xenophobic","Xenial","Xenodolnial"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(xlist))
+		elif char1=="Y":
+			ylist=["Young","Yucky","Yeasty","Youthful","Yoda"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(ylist))
+		elif char1=="Z":
+			zlist=["Zestful","Zippy","Zoftig","Zasy","Zealous"]
+			print(name1[i],":\t",end=" ")
+			print(random.choice(zlist))
+	for j in range(0,35):
+		print("+×",end="")
+elif choice==2:
+		exit()
+		
+			
+			
+		
